27111e95f313d3ec3bb542e9309f6ccf5706531b
Nick <niiickolay@gmail.com>
2019-07-29 10:47:43 +0300

    Refactor messagesList

--- client/src/components/MessagesList/index.js
+++ client/src/components/MessagesList/index.js
@@ -2,74 +2,21 @@ import React from 'react';
 import { Query } from "react-apollo";
 import { Loader, Comment } from "semantic-ui-react";
 import Message from '../Message';
-import {
-  NEW_MESSAGE_SUBSCRIPTION,
-  MESSAGE_QUERY,
-  UPDATE_MESSAGE_SUBSCRIPTION,
-  NEW_REPLY_SUBSCRIPTION
-} from '../../queries';
+import { MESSAGE_QUERY } from '../../queries';
+import * as subscribe from './subscribe';
 
 const MessagesList = props => {
   const { orderBy, filter } = props;
 
-  const _subscribeToNewMessages = subscribeToMore => {
-    subscribeToMore({
-      document: NEW_MESSAGE_SUBSCRIPTION,
-      updateQuery: (prev, { subscriptionData }) => {
-        if (!subscriptionData.data) return prev;
-
-        const { newMessage } = subscriptionData.data;
-        const exists = prev.messages.messagesList.find(({ id }) => id === newMessage.id);
-        if (exists) return prev;
-
-        return {
-          ...prev, messages: {
-            messagesList: [newMessage, ...prev.messages.messagesList],
-            count: prev.messages.messagesList.length + 1,
-            __typename: prev.messages.__typename
-          }
-        };
-      }
-    });
-  };
-
-  const _subscribeToUpdateMessage = subscribeToMore => {
-    subscribeToMore({
-      document: UPDATE_MESSAGE_SUBSCRIPTION
-    });
-  };
-
-  const _subscribeToNewReply = subscribeToMore => {
-    subscribeToMore({
-      document: NEW_REPLY_SUBSCRIPTION,
-      updateQuery: (prev, { subscriptionData }) => {
-        if (!subscriptionData.data) return prev;
-
-        const { newReply } = subscriptionData.data;
-
-        const message = prev.messages.messagesList.find(
-          item => item.id === newReply.message.id
-        );
-        const exists = message.replies.find(({ id }) => id === newReply.id);
-        if (exists) return prev;
-
-        message.replies.push(newReply);
-
-        return {
-          ...prev
-        }
-      }
-    });
-  }
 
   return (
     <Query query={MESSAGE_QUERY} variables={{ orderBy, filter }}>
       {({ loading, error, data, subscribeToMore }) => {
         if (loading) return <Loader active inline='centered' />;
         if (error) return <div>Fetch error</div>;
-        _subscribeToNewMessages(subscribeToMore);
-        _subscribeToUpdateMessage(subscribeToMore);
-        _subscribeToNewReply(subscribeToMore);
+        subscribe.toNewMessages(subscribeToMore);
+        subscribe.toUpdateMessage(subscribeToMore);
+        subscribe.toNewReply(subscribeToMore);
 
         const { messages: { messagesList } } = data;
 
---
+++ client/src/components/MessagesList/subscribe.js
@@ -0,0 +1,55 @@
+import {
+  NEW_MESSAGE_SUBSCRIPTION,
+  UPDATE_MESSAGE_SUBSCRIPTION,
+  NEW_REPLY_SUBSCRIPTION
+} from '../../queries';
+
+export const toNewMessages = subscribeToMore => {
+  subscribeToMore({
+    document: NEW_MESSAGE_SUBSCRIPTION,
+    updateQuery: (prev, { subscriptionData }) => {
+      if (!subscriptionData.data) return prev;
+
+      const { newMessage } = subscriptionData.data;
+      const exists = prev.messages.messagesList.find(({ id }) => id === newMessage.id);
+      if (exists) return prev;
+
+      return {
+        ...prev, messages: {
+          messagesList: [newMessage, ...prev.messages.messagesList],
+          count: prev.messages.messagesList.length + 1,
+          __typename: prev.messages.__typename
+        }
+      };
+    }
+  });
+};
+
+export const toUpdateMessage = subscribeToMore => {
+  subscribeToMore({
+    document: UPDATE_MESSAGE_SUBSCRIPTION
+  });
+};
+
+export const toNewReply = subscribeToMore => {
+  subscribeToMore({
+    document: NEW_REPLY_SUBSCRIPTION,
+    updateQuery: (prev, { subscriptionData }) => {
+      if (!subscriptionData.data) return prev;
+
+      const { newReply } = subscriptionData.data;
+
+      const message = prev.messages.messagesList.find(
+        item => item.id === newReply.message.id
+      );
+      const exists = message.replies.find(({ id }) => id === newReply.id);
+      if (exists) return prev;
+
+      message.replies.push(newReply);
+
+      return {
+        ...prev
+      }
+    }
+  });
+}
